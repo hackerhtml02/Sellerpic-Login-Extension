3c8c2432d6750d70af35128e54f8c55a01ebf73a
hackerhtml02 <97086315+hackerhtml02@users.noreply.github.com>
2025-01-30 18:55:00 +0500

    Update content.js

--- content.js
+++ content.js
@@ -1,16 +1,7 @@
-var element = document.evaluate('/html/body/div[1]/div/header/div/div/div[1]/section/div/button', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+var button = document.evaluate("//*[@id='root']/div/div/div[2]/div[1]/form/button[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
-if (element) {
-    element.click();
+if (button) {
+    button.click();
 } else {
-    console.log("Element not found!");
+    console.error("Button not found!");
 }
-setTimeout(function() {
-    var element = document.evaluate('/html/body/div[2]/div/div[2]/div/div[1]/section/section[2]/div[2]/section/a', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-    if (element) {
-        element.click();
-    } else {
-        console.log("Element not found!");
-    }
-}, 2000); // 2000 milliseconds = 2 seconds
